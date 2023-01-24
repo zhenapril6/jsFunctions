2a359918029c24d474596b51b24b5b90e8b3c4a4
Yauheniya Stsiapanava <zhenapril6@gmail.com>
2023-01-24 22:04:34 +0300

    feat: update js file

--- functions.js
+++ functions.js
@@ -1,16 +1,41 @@
-function sum(a, b) {
-    console.log(a, b)
+function callbackFunc() {
+    console.log('callback');
+}
+
+function sum(a, b, callback) {
+    callback();
     return a + b;
 }
+
+const numbersSum = sum(4, 6, function() {
+    console.log('newcallback');
+});
+
+console.log(numbersSum)
+
+
+
+
+
 
 const sum2 = function(a, b) {
     return a + b;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const sum3 = (a, b) => {
     return a + b;
 }
-
-const numbersSum = sum3(4, 6);
-
-console.log(numbersSum)
